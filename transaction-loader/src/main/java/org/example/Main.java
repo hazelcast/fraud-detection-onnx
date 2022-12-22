0f43c23f064d457beff0df29c9656c61a26d6cc8
Ed Sandoval <esandoval@HZLCST-MBP-95.local>
2022-12-22 13:14:01 +0000

    updating transaction loader

--- transaction-loader/src/main/java/org/example/Main.java
+++ transaction-loader/src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hazelcast.config.Config;
-import com.hazelcast.core.Hazelcast;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.jet.config.JobConfig;
-import com.hazelcast.jet.core.JobStatus;
 import com.hazelcast.jet.kafka.KafkaSinks;
 import com.hazelcast.jet.pipeline.BatchSource;
 import com.hazelcast.jet.pipeline.Pipeline;
@@ -14,43 +13,61 @@ import com.hazelcast.jet.pipeline.file.FileSources;
 import datamodel.Transaction;
 import org.apache.kafka.common.serialization.StringSerializer;
 import java.util.AbstractMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 
 public class Main {
 
 
-    private static HazelcastInstance startHazelcastInstances() {
 
-        Config cfg = new Config();
-        cfg.getNetworkConfig().setPort( 25701 ).setPortAutoIncrement(false);
-        cfg.getJetConfig().setEnabled(true);
-        HazelcastInstance hazelcastInstance =  Hazelcast.newHazelcastInstance(cfg);
-        hazelcastInstance.getJet().getConfig().setResourceUploadEnabled(true);
-        return hazelcastInstance;
+    private static HazelcastInstance getHazelClient(String hazelcastClusterMemberAddresses)  {
 
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setClusterName("dev");
+        clientConfig.getNetworkConfig().addAddress(hazelcastClusterMemberAddresses);
+
+        //Start the client
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+        System.out.println("Connected to Hazelcast Cluster");
+        return client;
     }
+
+    private static final String TRANSACTION_FOLDER= "/opt/hazelcast/transaction";
     public static void main(String[] args) throws InterruptedException {
 
-        HazelcastInstance hazelcastInstance = startHazelcastInstances();
-        Thread.sleep(5000);
+        Map<String, String> env = System.getenv();
+        String transactionFileName = args[0];
+        String HZ_ONNX = env.get("HZ_ONNX");
+        String KAFKA_CLUSTER_KEY = env.get("KAFKA_CLUSTER_KEY");
+        String KAFKA_CLUSTER_SECRET = env.get("KAFKA_CLUSTER_SECRET");
+        String KAFKA_CLUSTER_ENDPOINT = env.get("KAFKA_ENDPOINT");
 
-        if (args!=null && args.length ==3) {
-            submitTransactionLoaderJob(args[0],args[1],args[2],hazelcastInstance,"transactionLoader");
+        HazelcastInstance client = getHazelClient(HZ_ONNX);
+        Thread.sleep(3000);
 
-            //Sleep while job is running
-            while (! hazelcastInstance.getJet().getJob("transactionLoader").getStatus().equals(JobStatus.COMPLETED)) {
-                Thread.sleep(3000);
-            }
+        if (!transactionFileName.isBlank()) {
+
+            submitTransactionLoaderJob(TRANSACTION_FOLDER,
+                    transactionFileName,
+                    KAFKA_CLUSTER_ENDPOINT
+                    ,client,
+                    "transactionLoader",
+                    KAFKA_CLUSTER_KEY,
+                    KAFKA_CLUSTER_SECRET);
+
+            System.out.println("Transaction Loading Job submitted to Hazelcast");
+
 
         } else {
-            System.out.println("Expected 3 command line arguments: <transactionFileFolder> <transactionFileName> <kafkaserver:port>");
+            System.out.println("Unable to submit job. Please check environment variables are set and file name is correct.");
         }
 
-        hazelcastInstance.shutdown();
+        client.shutdown();
 
 
     }
-    private static void submitTransactionLoaderJob (String transactionFolder, String transactionFilename, String bootstrapServers,HazelcastInstance hazelcastInstance,String jobName) {
+    private static void submitTransactionLoaderJob (String transactionFolder, String transactionFilename, String bootstrapServers,HazelcastInstance hazelcastInstance,String jobName,String clusterKey, String clusterSecret) {
         //Batch Source = is a CSV File
         BatchSource<Transaction> source = FileSources.files(transactionFolder)
                 .glob(transactionFilename)
@@ -67,25 +84,36 @@ public class Main {
                     String jsonString = mapper.writeValueAsString(transaction);
                     return new AbstractMap.SimpleEntry<>(key, jsonString);
                 } )
-                .writeTo(KafkaSinks.kafka(getKafkaBrokerProperties(bootstrapServers), "Transactions"));
+                .writeTo(KafkaSinks.kafka(getKafkaBrokerProperties(bootstrapServers,clusterKey,clusterSecret), "Transactions"));
         //Execute the Job
         submitJob(pipeline,hazelcastInstance,jobName);
     }
-    private static Properties getKafkaBrokerProperties (String bootstrapServers) {
+    private static Properties getKafkaBrokerProperties (String bootstrapServers, String clusterKey, String clusterSecret) {
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", bootstrapServers);
         props.setProperty("key.serializer", StringSerializer.class.getCanonicalName());
         props.setProperty("value.serializer", StringSerializer.class.getCanonicalName());
+        //Cloud
+        props.setProperty("security.protocol","SASL_SSL");
+        props.setProperty("sasl.jaas.config","org.apache.kafka.common.security.plain.PlainLoginModule required username='" + clusterKey  + "' password='" + clusterSecret  +"';");
+        props.setProperty("sasl.mechanism","PLAIN");
+        props.setProperty("session.timeout.ms","45000");
+        props.setProperty("client.dns.lookup","use_all_dns_ips");
+        props.setProperty("acks","all");
+
         return props;
     }
 
-    private static void submitJob (Pipeline p, HazelcastInstance hazelcastInstance, String jobName)  {
+    private static void submitJob (Pipeline p, HazelcastInstance hazelcastInstance,String jobName)  {
 
         JobConfig jobCfg = new JobConfig();
-        jobCfg.setName(jobName);
+        jobCfg.setName(jobName + UUID.randomUUID());
         jobCfg.addClass(Transaction.class);
         jobCfg.addClass(Main.class);
+
         hazelcastInstance.getJet().newJob(p, jobCfg);
+
+
     }
 
 
