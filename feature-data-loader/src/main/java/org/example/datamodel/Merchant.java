063114af431a5cff3bded1a56e4b826a42056ea4
Ed Sandoval <esandoval@HZLCST-MBP-95.local>
2023-06-06 15:03:21 +0100

    compact

--- feature-data-loader/src/main/java/org/example/datamodel/Merchant.java
+++ feature-data-loader/src/main/java/org/example/datamodel/Merchant.java
@@ -1,13 +1,8 @@
 package org.example.datamodel;
 
 
-import com.hazelcast.nio.serialization.Portable;
-import com.hazelcast.nio.serialization.PortableReader;
-import com.hazelcast.nio.serialization.PortableWriter;
 
-import java.io.IOException;
-
-public class Merchant implements Portable {
+public class Merchant  {
     private String category;
     private String merchant_name;
     private Long merchantCode;
@@ -47,30 +42,4 @@ public class Merchant implements Portable {
         return (header + name + category + code);
     }
 
-    final static int CLASS_ID = 1;
-    final static int FACTORY_ID = 1;
-    @Override
-    public int getFactoryId() {
-        return FACTORY_ID;
-    }
-
-    @Override
-    public int getClassId() {
-        return CLASS_ID;
-    }
-
-    @Override
-    public void writePortable(PortableWriter writer) throws IOException {
-        writer.writeString("category", category);
-        writer.writeString("merchant_name", merchant_name);
-        if (merchantCode!=null) {
-            writer.writeLong("merchantCode", merchantCode.longValue());
-        }
-    }
-    @Override
-    public void readPortable(PortableReader reader) throws IOException {
-       category =  reader.readString("category");
-       merchant_name = reader.readString("merchant_name");
-       merchantCode = Long.valueOf(reader.readLong("merchantCode"));
-    }
 }
